5c76d348dfe673b2946b5c92ba234da1113b6b94
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2021-06-16 23:43:38 -0300

    upload portfolio

---
+++ README.md
@@ -0,0 +1,61 @@
+ENGLISH
+
+# Portfolio FS
+This is my personal **portfolio**. <br>
+In 2020 I began to study programming on my own, from platforms such as Udemy, FreeCodeCamp, w3schools, DevCode, SoloLearn, LinkedIn Learning and many hours practicing. <br>
+I am currently studying **Full Stack** with Programa desde tu Casa, Codo a Codo and Egg. <br>
+I plan to keep learning and keep up to date. 
+
+## Content 🚀
+This Project only has one master branch, it was a small project so it did not need more branches.
+
+## Stack of technologies?  🛠️
+
+**HTML5** , **CSS3** and **JavaScript**
+
+## Demo 📦
+If you want to see how this project was, you can do it from:
+:arrow_right:   https://eugenia1984.github.io/Portafolio
+
+## Please: 🎁
+
+* Share this proyect with others 📢
+* Invite a beer 🍺 or a coffee ☕  to someone of the team. 
+* Thanks a lot 🤓.
+
+---
+## Notes
+I appreciate your interest in this Project <br/>
+⌨️ with ❤️ by [María Eugenia Costa](https://github.com/eugenia1984) 😊 and follow me at [LinkedIn](http://www.linkedin.com/in/maríaeugeniacosta) 
+---
+
+ESPAÑOL
+
+# Portafolio
+Este es mi **portafolio** personal. <br>
+En 2020 comencé a estudiar programación por mi cuenta, desde plataformas como Udemy, FreeCodeCamp, w3schools, DevCode, SoloLearn, LinkedIn Learning y muchas horas practicando. <br>
+Actualmente estoy estudiando **Full Stack** con Programa desde tu Casa, Codo a Codo y Egg. <br>
+Planeo seguir aprendiendo y mantenerme actualizado.
+
+## Contenido 🚀
+Este proyecto solo tiene una sucursal maestra, era un proyecto pequeño por lo que no necesitaba más sucursales.
+
+## Stack de tecnologías 🛠️
+
+**HTML5**, **CSS3** y **JavaScript**
+
+## Demo 📦
+Si quieres ver cómo fue este proyecto, puedes hacerlo desde:
+:arrow_right: https://eugenia1984.github.io/Portafolio
+
+## Por favor: 🎁
+
+* Comparte este proyecto con otros 📢
+* Invita una cerveza 🍺 o un café ☕ a alguien del equipo.
+* Muchas gracias 🤓.
+
+---
+## Notas
+Agradezco su interés en este proyecto <br/>
+⌨️ con ❤️ de [María Eugenia Costa](https://github.com/eugenia1984) 😊 y sígueme en [LinkedIn](http://www.linkedin.com/in/maríaeugeniacosta)
+---
---
+++ js/script.js
@@ -0,0 +1,83 @@
+/******Menu*******/
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+
+    $btnMenu.addEventListener("click", (e) => {
+      $btnMenu.firstElementChild.classList.toggle("none");
+      $btnMenu.lastElementChild.classList.toggle("none");
+      $menu.classList.toggle("is-active");
+    });
+    
+    d.addEventListener("click", (e) => {
+      if (!e.target.matches(".menu a")) return false;
+  
+      $btnMenu.firstElementChild.classList.remove("none");
+      $btnMenu.lastElementChild.classList.add("none");
+      $menu.classList.remove("is-active");
+    });
+  })(document);
+
+  /* ********** Formulario de contacto********** */
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/costamariaeugenia1@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+})(document);
+
+/*Slider de certificados*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
